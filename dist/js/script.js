9de45570c30b22a2b123d1615603bbfbd1ad0ab7
Olga_Michalyata <olgamikhalyata.com@mail.ru>
2022-08-06 21:26:15 +0200

    06.08.22

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__slider_percent'),
+         lines = document.querySelectorAll('.skills__slider_slider span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+})  
