02c688d8ffd3ba3e8cc39ba34b3485e974c06d70
Olga_Michalyata <olgamikhaluata.com@mail.ru>
2023-03-19 02:00:51 +0100

    page translate into 3 languages 19.03.23

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger'),
-        menu = document.querySelector('.menu'),
-        closeElem = document.querySelector('.menu__close');
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close'),
+      menuLinks = document.querySelectorAll('.menu__link');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -8,4 +9,10 @@ hamburger.addEventListener('click', () => {
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
+});
+
+menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
 });
